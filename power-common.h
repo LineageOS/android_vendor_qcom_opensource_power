8b891bba1b9668864554b61759a5380e87def2a1
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-06-26 11:37:59 +0800

    power: Use ARRAY_SIZE macro where possible
    Change-Id: Ie47d0e6b91b7bba378b0fdf53a345a021b0c2d48

--- power-common.h
+++ power-common.h
@@ -52,6 +52,7 @@ void power_hint(power_hint_t hint, void* data);
 void set_interactive(int on);
 void set_feature(feature_t feature, int state);
 
+#define ARRAY_SIZE(x) (sizeof((x)) / sizeof((x)[0]))
 #define UNUSED(x) UNUSED_##x __attribute__((__unused__))
 
 #ifdef __cplusplus
