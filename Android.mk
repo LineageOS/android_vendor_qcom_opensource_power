825fbdc31b03b111609799efebe0984c76c9ad64
dianlujitao <dianlujitao@lineageos.org>
2018-01-18 21:24:30 +0800

    power: Prepare for power profile support
     * Add support for system performance profiles and CPU boost.
     * Uses mpctl to configure the CPUs in the appropriate modes.
     * Implement binderized Lineage power HAL.
    
    Author: Steve Kondik <shade@chemlab.org>
    Date:   Sat May 17 03:37:53 2014 -0700
    
        power: Add support for CPU boost hint
    
        Change-Id: I07c3e8daf8a5f3b568e27334f26d4ba8f4cf40c4
    
    Author: Steve Kondik <shade@chemlab.org>
    Date:   Sun May 18 23:55:23 2014 -0700
    
        power: Add power hint to set profile
    
         * A PowerHAL can implement support for this hint to receive
           power profile changes from the framework.
    
        Change-Id: Ie1e9e3b827c731cf5a817a0491677e3451fe8678
    
    Author: Steve Kondik <shade@chemlab.org>
    Date:   Mon May 19 17:26:34 2014 -0700
    
        power: Add support for SET_PROFILE hints
    
         * Add support for system performance profiles.
         * Uses mpctl to configure the CPUs in the appropriate modes.
         * PERFORMANCE = all cores at max
         * POWER_SAVE = max two cores at non-turbo voltage
         * If a custom profile is set, don't honor any other hints
         * Clean up the code and firm up the locking.
    
        Change-Id: Ie6acada805780c9ae6e6bc2002843aef638ca63b
    
    Author: Steve Kondik <steve@cyngn.com>
    Date:   Tue Nov 3 03:27:42 2015 -0800
    
        power: Update for PerformanceManager changes
    
         * Undo damage caused by the previous patch which broke
           all hint arguments. We were actually using these wrong to
           begin with anyway.
         * Add support for get_profile
         * Clean up code
    
        Change-Id: Ibc3f21bfb7aa46ec97b9b63d09737d4331a5a714
    
    Author: dianlujitao <dianlujitao@lineageos.org>
    Date:   Sat Feb 23 20:24:57 2019 +0800
    
        power: Pass NULL parameter in powerHint if data is zero
    
         * This restores the behavior in AOSP and CAF power HAL to avoid
           confusion.
    
        Change-Id: I72f5bb9286e2f57121e39eea82d2fe8854989393
    
    Change-Id: I7ae614667e3182eaf0a90b81e8ed839689cfbf28

--- Android.mk
+++ Android.mk
@@ -1,3 +1,31 @@
+# Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
+# Copyright (C) 2019 The LineageOS Project
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+# *    * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#     * Neither the name of The Linux Foundation nor the names of its
+#       contributors may be used to endorse or promote products derived
+#       from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+# BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
@@ -8,6 +36,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libhidlbase libhidltransport libhwbinder libutils android.hardware.power@1.2
+LOCAL_SHARED_LIBRARIES += vendor.lineage.power@1.0
 LOCAL_HEADER_LIBRARIES += libhardware_headers
 LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c service.cpp Power.cpp
 LOCAL_C_INCLUDES := external/libxml2/include \
