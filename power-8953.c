caeca588cc55aaa9e0bc8b56d4500b454c189c3c
Zhao Wei Liew <zhaoweiliew@gmail.com>
2018-11-02 00:33:14 +0000

    power: Cache SOC ID checks for future queries
    Currently, get_soc_id() is queried on every SOC ID check in the
    SDM660, MSM8916, MSM8952, MSM8953 and MSM8974-family HALs.
    This results in extraneous file operations on every SOC ID check.
    
    Cache the result of get_soc_id() during the first query to
    reduce the number of file operations being made.
    
    This also brings back the behaviour in LineageOS 15.1 when the
    HALs were still stored in device/qcom/common.
    
    Change-Id: Ic17dbf12e7f9ecdb47b73a580f467df9ad630aa0

--- power-8953.c
+++ power-8953.c
@@ -57,17 +57,16 @@ static int video_encode_hint_sent;
 static void process_video_encode_hint(void* metadata);
 
 /**
- * If target is SDM632:
- *     return true
- * else:
- *     return false
+ * Returns true if the target is SDM632.
  */
 static bool is_target_SDM632(void) {
-    static bool is_SDM632 = false;
+    static int is_SDM632 = -1;
     int soc_id;
 
+    if (is_SDM632 >= 0) return is_SDM632;
+
     soc_id = get_soc_id();
-    if (soc_id == 349 || soc_id == 350) is_SDM632 = true;
+    is_SDM632 = soc_id == 349 || soc_id == 350;
 
     return is_SDM632;
 }
