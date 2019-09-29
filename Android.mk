c4adf906e6c20f8b504f48af31badbd7261d05fb
Michael Bestas <mkbestas@lineageos.org>
2019-09-29 23:59:05 +0300

    power: clang-format
    * Using AOSP interface .clang-format
    * Clean Android.mk while we are at it
    
    Change-Id: I630f72e3dffb676ca1930e72945e897f62103ada
    Signed-off-by: Arian <arian.kulmer@web.de>

--- Android.mk
+++ Android.mk
@@ -2,54 +2,69 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(call is-vendor-board-platform,QCOM),true)
 
-# HAL module implemenation stored in
-# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_RELATIVE_PATH := hw
-# KEYSTONE(I1132378f14428bf511f3cea4f419e90a6e89f823,b/181709127)
-LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libbase libutils android.hardware.power-V1-ndk_platform libbinder_ndk
-LOCAL_HEADER_LIBRARIES += libhardware_headers
-LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c Power.cpp main.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libdl \
+    libbase \
+    libutils \
+    android.hardware.power-V1-ndk_platform \
+    libbinder_ndk
+
+LOCAL_HEADER_LIBRARIES := \
+    libhardware_headers
+
+LOCAL_SRC_FILES := \
+    power-common.c \
+    metadata-parser.c \
+    utils.c \
+    list.c \
+    hint-data.c \
+    Power.cpp \
+    main.cpp
 
 ifneq ($(BOARD_POWER_CUSTOM_BOARD_LIB),)
     LOCAL_WHOLE_STATIC_LIBRARIES += $(BOARD_POWER_CUSTOM_BOARD_LIB)
 else
 
 # Include target-specific files.
-ifeq ($(call is-board-platform-in-list, msm8974), true)
+ifeq ($(call is-board-platform-in-list,msm8974), true)
 LOCAL_SRC_FILES += power-8974.c
 endif
 
-ifeq ($(call is-board-platform-in-list, msm8226), true)
+ifeq ($(call is-board-platform-in-list,msm8226), true)
 LOCAL_SRC_FILES += power-8226.c
 endif
 
-ifeq ($(call is-board-platform-in-list, msm8610), true)
+ifeq ($(call is-board-platform-in-list,msm8610), true)
 LOCAL_SRC_FILES += power-8610.c
 endif
 
-ifeq ($(call is-board-platform-in-list, apq8084), true)
+ifeq ($(call is-board-platform-in-list,apq8084), true)
 LOCAL_SRC_FILES += power-8084.c
 endif
 
-ifeq ($(call is-board-platform-in-list, msm8909), true)
+ifeq ($(call is-board-platform-in-list,msm8909), true)
 LOCAL_SRC_FILES += power-8909.c
 endif
 
-ifeq ($(call is-board-platform-in-list, msm8916), true)
+ifeq ($(call is-board-platform-in-list,msm8916), true)
 LOCAL_SRC_FILES += power-8916.c
 endif
 
-ifeq ($(call is-board-platform-in-list, msm8992), true)
+ifeq ($(call is-board-platform-in-list,msm8992), true)
 LOCAL_SRC_FILES += power-8992.c
 endif
 
-ifeq ($(call is-board-platform-in-list, msm8994), true)
+ifeq ($(call is-board-platform-in-list,msm8994), true)
 LOCAL_SRC_FILES += power-8994.c
 endif
 
-ifeq ($(call is-board-platform-in-list, msm8996), true)
+ifeq ($(call is-board-platform-in-list,msm8996), true)
 LOCAL_SRC_FILES += power-8996.c
 endif
 
@@ -89,7 +104,7 @@ ifeq ($(call is-board-platform-in-list,msmnile), true)
 LOCAL_SRC_FILES += power-msmnile.c
 endif
 
-endif  #  End of board specific list
+endif # End of board specific list
 
 ifneq ($(TARGET_POWERHAL_SET_INTERACTIVE_EXT),)
     LOCAL_CFLAGS += -DSET_INTERACTIVE_EXT
@@ -110,5 +125,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS += -Wno-unused-parameter -Wno-unused-variable
 LOCAL_VENDOR_MODULE := true
 LOCAL_VINTF_FRAGMENTS := power.xml
+
 include $(BUILD_EXECUTABLE)
 endif
