ce5b68496a695523381a4f8982f2ae70d4d61f9d
Michael Bestas <mkbestas@lineageos.org>
2019-09-29 21:06:22 +0300

    power: Add -qti suffix to module name
    * Prevent conflict with other HALs
    
    Change-Id: Icf1216410db26c3bb811d7aedde03e6d2f87e3cc

--- Power.cpp
+++ Power.cpp
@@ -27,7 +27,7 @@
  * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#define LOG_TAG "QTI PowerHAL"
+#define LOG_TAG "android.hardware.power@1.2-service-qti"
 
 #include <log/log.h>
 #include "Power.h"
