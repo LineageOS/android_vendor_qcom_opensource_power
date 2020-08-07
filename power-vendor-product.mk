12595501b7398e61434b53eecd97ca3cf5f72920
Shayak Biswas <shaybisw@codeaurora.org>
2020-08-07 11:42:30 +0530

    Power : Modified make file to include Bengal
    Addition of powerhint.xml for the product Bengal
    
    Change-Id: If389dedd70bb76e08a842775350eda091ce7cddf

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -13,6 +13,8 @@ else ifeq ($(TARGET_BOARD_PLATFORM),atoll)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/atoll/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),sm6150)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/sm6150/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),bengal)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/bengal/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),lahaina)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),holi)
