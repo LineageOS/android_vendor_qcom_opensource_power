777f97d17b2baf3cf48122eb9663b238c67d3ec0
Vini Vennapusa <vvini@codeaurora.org>
2021-07-01 18:33:35 +0530

    Power: Add powerhint.xml for waipio
    Added powerhint.xml for waipio target and
    modified the make file.
    
    Change-Id: Ib28f9f2b00ef116394b0a88cf60a6d004c23b5bc

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -15,4 +15,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),lahaina)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/lahaina/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),holi)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/holi/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),taro)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
