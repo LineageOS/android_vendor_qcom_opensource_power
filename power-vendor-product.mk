0f893c2254af1011126b4bc660724b2ba339167b
Om SIngh <quic_omsingh@quicinc.com>
2022-05-11 15:48:31 +0530

    power: added powerhint.xml for neo target
    Added powerhint for neo
    
    Change-Id: I833338abe6ef8ddfd6974c2e0b4347a8dd7c8399

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -21,4 +21,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),kalama)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/kalama/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),parrot)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/parrot/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),neo)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/neo/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
