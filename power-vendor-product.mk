5637f2e74f0e932994a7e54871346a9b575c8386
zehugong <quic_zehugong@quicinc.com>
2023-01-05 11:08:57 +0800

    power: Add powerhint.xml for pineapple.
    Added powerhint.xml for pineapple target.
    
    Change-Id: Ib7bdecd134418adf98ab5829d636925fcace9638

--- power-vendor-product.mk
+++ power-vendor-product.mk
@@ -19,4 +19,6 @@ else ifeq ($(TARGET_BOARD_PLATFORM),taro)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/taro/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 else ifeq ($(TARGET_BOARD_PLATFORM),kalama)
 PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/kalama/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+else ifeq ($(TARGET_BOARD_PLATFORM),pineapple)
+PRODUCT_COPY_FILES += vendor/qcom/opensource/power/config/pineapple/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 endif
