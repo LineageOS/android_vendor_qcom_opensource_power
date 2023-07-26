af433c201cdfd98e6a0686b1cb76c902e8e9c663
Vladimir Mikhailov <beykerykt@gmail.com>
2023-07-26 20:38:27 +0900

    power: Move launch and interaction hint handlers to common
    Qualcomm has been using MPCTL to boost their SoCs for a long time.
    Move interaction and launch hints to a common power_hint for support
    boost in current and future SoCs.
    
    We do not touch legacy SoCs, as these do not support MPCTLv3.
    
    Change-Id: I35e301edc0c67782f38e3d293719362e68233b56

--- Android.mk
+++ Android.mk
@@ -106,10 +106,6 @@ ifeq ($(call is-board-platform-in-list,qcs605), true)
 LOCAL_SRC_FILES += power-710.c
 endif
 
-ifeq ($(call is-board-platform-in-list,msmnile), true)
-LOCAL_SRC_FILES += power-msmnile.c
-endif
-
 endif # End of board specific list
 
 ifneq ($(TARGET_POWERHAL_MODE_EXT),)
