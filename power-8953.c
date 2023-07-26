d74469fd1edcf44f623ad266fc6d97bf7fb7d586
Vladimir Mikhailov <beykerykt@gmail.com>
2023-07-26 20:38:27 +0900

    power: Move launch and interaction hint handlers to common
    Qualcomm has been using MPCTL to boost their SoCs for a long time.
    Move interaction and launch hints to a common power_hint for support boost in current and future SoCs.
    
    We do not touch legacy SoCs, as these do not support MPCTLv3.
    
    Change-Id: I35e301edc0c67782f38e3d293719362e68233b56

--- power-8953.c
+++ power-8953.c
@@ -52,10 +52,6 @@
 
 static int video_encode_hint_sent;
 
-const int kMinInteractiveDuration = 500;  /* ms */
-const int kMaxInteractiveDuration = 5000; /* ms */
-const int kMaxLaunchDuration = 5000;      /* ms */
-
 /**
  * Returns true if the target is SDM632.
  */
@@ -155,80 +151,11 @@ static int process_video_encode_hint(void* metadata) {
     return HINT_NONE;
 }
 
-static void process_interaction_hint(void* data) {
-    static struct timespec s_previous_boost_timespec;
-    static int prev_interaction_handle = -1;
-
-    struct timespec cur_boost_timespec;
-    long long elapsed_time;
-    int duration = kMinInteractiveDuration;
-
-    if (data) {
-        int input_duration = *((int*)data);
-        if (input_duration > duration) {
-            duration = (input_duration > kMaxInteractiveDuration) ? kMaxInteractiveDuration
-                                                                  : input_duration;
-        }
-    }
-
-    clock_gettime(CLOCK_MONOTONIC, &cur_boost_timespec);
-
-    elapsed_time = calc_timespan_us(s_previous_boost_timespec, cur_boost_timespec);
-    // don't hint if it's been less than 250ms since last boost
-    // also detect if we're doing anything resembling a fling
-    // support additional boosting in case of flings
-    if (elapsed_time < 250000 && duration <= 750) {
-        return;
-    }
-    s_previous_boost_timespec = cur_boost_timespec;
-
-    int interaction_handle =
-            perf_hint_enable_with_type(VENDOR_HINT_SCROLL_BOOST, duration, SCROLL_VERTICAL);
-
-    if (CHECK_HANDLE(prev_interaction_handle)) {
-        release_request(prev_interaction_handle);
-    }
-    prev_interaction_handle = interaction_handle;
-}
-
-static int process_activity_launch_hint(void* data) {
-    static int launch_handle = -1;
-    static int launch_mode = 0;
-
-    // release lock early if launch has finished
-    if (!data) {
-        if (CHECK_HANDLE(launch_handle)) {
-            release_request(launch_handle);
-            launch_handle = -1;
-        }
-        launch_mode = 0;
-        return HINT_HANDLED;
-    }
-
-    if (!launch_mode) {
-        launch_handle = perf_hint_enable_with_type(VENDOR_HINT_FIRST_LAUNCH_BOOST,
-                                                   kMaxLaunchDuration, LAUNCH_BOOST_V1);
-        if (!CHECK_HANDLE(launch_handle)) {
-            ALOGE("Failed to perform launch boost");
-            return HINT_NONE;
-        }
-        launch_mode = 1;
-    }
-    return HINT_HANDLED;
-}
-
 int power_hint_override(power_hint_t hint, void* data) {
     int ret_val = HINT_NONE;
     switch (hint) {
         case POWER_HINT_VIDEO_ENCODE:
             ret_val = process_video_encode_hint(data);
-            break;
-        case POWER_HINT_INTERACTION:
-            process_interaction_hint(data);
-            ret_val = HINT_HANDLED;
-            break;
-        case POWER_HINT_LAUNCH:
-            ret_val = process_activity_launch_hint(data);
             break;
         default:
             break;
